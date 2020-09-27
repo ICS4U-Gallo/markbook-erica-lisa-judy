416d02a66937773afb82d7f5a3cb2d93cc4d4336
Erica Han <erica.han22@ycdsbk12.ca>
2020-09-27 19:44:52 +0000

    Create Classroom & Average Mark Calc

--- markbook.py
+++ markbook.py
@@ -20,12 +20,35 @@ def create_assignment(name: str, due: str, points: int) -> Dict:
 
 def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
     """Creates a classroom dictionary"""
-    return {}
+
+course_code = input("Enter course code: ")
+course_name = input("Enter course name: ")
+period_nmb = int(input("Enter the period: "))
+teacher_name = input("Enter the teacher's name: ")
+
+classroom = { 
+    "Course code" : course_code , 
+    "Course name" : course_name , 
+    "Period" : period_nmb , 
+    "Teacher's name" : teacher_name 
+    }
+print(classroom)
 
 
 def calculate_average_mark(student: Dict) -> float:
     """Calculates the average mark of a student"""
-    return 0
+
+name_student = input("What is the student's full name? \n")
+no_of_grds = int(input("How many grades are being inputted? \n"))
+total_sum = 0
+
+for n in range(no_of_grds) :
+    grades = float(input("Enter mark: "))
+    total_sum += grades
+
+avg = total_sum/no_of_grds
+
+print(name_student , "'s average mark is" , avg)
 
 
 student = {'First Name': "unknown", 'Last Name': "unknown"}
