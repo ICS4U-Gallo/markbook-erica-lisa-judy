2d48f40237f449ba534af24cbdaec7ae7ad3cbf6
LisaTan <50634813+LisaTOVO@users.noreply.github.com>
2020-09-29 03:38:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from markbook import *
 import os
+
+print("======================")
+print("Welcom to markbook")
+print(""=====================")
+
+print("Enter 0 to create an assignment")
+print("Enter 1 to check assignment details")
+print("Enter 2 to add a student")
+print("Enter 3 to edit student")
+      
+user_input1 = int(input("Enter a number between 0-3: ")
+
+if user_input1 == 0
